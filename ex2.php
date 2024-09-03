c538cf2f0aad8ae071159013d0602084a548c3b0
Ankitherma <hermaankit@gmail.com>
2024-09-03 23:19:24 +0530

    Task1

---
+++ ex2.php
@@ -0,0 +1,8 @@
+<?php
+
+$name="ankit";
+$age="20";
+echo "My name is $name<br>";
+echo " i am $age years old";
+
+?>
---
+++ ex9.php
@@ -0,0 +1,3 @@
+<?php
+    echo "Name Ankit<br>City : Rajkot<br> Collage : Atmiya University<br>Contact :8877669941<br>Department :MCA <br> Div :A"
+?>
